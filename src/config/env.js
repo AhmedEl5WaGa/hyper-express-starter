d557efce614b24d2ec5e3568d5f108f11932e5e6
Ahmed Fawzy <ahmed.fawzy100150@gmail.com>
2024-07-03 20:08:16 +0300

    fully tested api with db and global error handling

--- src/config/env.js
+++ src/config/env.js
@@ -7,27 +7,6 @@ const envVarsSchema = Joi.object()
       .required(),
     PORT: Joi.number().default(4000),
     MONGODB_URL: Joi.string().required().description("Mongo DB url"),
-    JWT_SECRET: Joi.string().required().description("JWT secret key"),
-    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number()
-      .default(30)
-      .description("minutes after which access tokens expire"),
-    JWT_REFRESH_EXPIRATION_DAYS: Joi.number()
-      .default(30)
-      .description("days after which refresh tokens expire"),
-    JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description("minutes after which reset password token expires"),
-    JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description("minutes after which verify email token expires"),
-    SMTP_HOST: Joi.string().description("server that will send the emails"),
-    SMTP_PORT: Joi.number().description("port to connect to the email server"),
-    SMTP_USERNAME: Joi.string().description("username for email server"),
-    SMTP_PASSWORD: Joi.string().description("password for email server"),
-    EMAIL_FROM: Joi.string().description(
-      "the from field in the emails sent by the app"
-    ),
-    CLIENT_URL: Joi.string().required().description("Client url"),
   })
   .unknown();
 
